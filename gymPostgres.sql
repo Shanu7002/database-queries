cb38be957e649ce4389a7a8c9d9f5d9b600bec77
Edu Muller <eduardoaum2007@gmail.com>
2026-02-11 16:58:22 -0300

    feat: students table created

--- gymPostgres.sql
+++ gymPostgres.sql
@@ -1,5 +1,11 @@
 -- That's a gym database just for postgres training --
 -- Create and connect --
-CREATE DATABASE gym;
-\c gym
+-- CREATE DATABASE gym;
+-- \c gym
 
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    registration_date TIMESTAMP DEFAULT NOW()
+)
