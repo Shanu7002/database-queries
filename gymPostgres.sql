fbdf5694f607373817572a18e7457d7f087c1b9d
Edu Muller <eduardoaum2007@gmail.com>
2026-02-11 17:15:09 -0300

    feat: enrollments table created

--- gymPostgres.sql
+++ gymPostgres.sql
@@ -8,10 +8,22 @@ CREATE TABLE students (
     name TEXT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     registration_date TIMESTAMP DEFAULT NOW()
-)
+);
 
 CREATE TABLE memberships (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    price FLOAT
-)
+    price FLOAT CHECK (price >= 0)
+);
+
+CREATE TABLE enrollments (
+    id SERIAL PRIMARY KEY,
+    id_student INT NOT NULL,
+    id_membership INT NOT NULL,
+    start_date DATE DEFAULT CURRENT_DATE,
+    end_date DATE,
+    status VARCHAR(50),
+    FOREIGN KEY (id_student) REFERENCES students(id),
+    FOREIGN KEY (id_membership) REFERENCES memberships(id),
+    CHECK (end_date IS NULL OR end_date >= start_date)
+);
