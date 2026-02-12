817fe6b5dc9c80eaf32485114bc3da89ec1f96ea
Edu Muller <eduardoaum2007@gmail.com>
2026-02-12 15:17:54 -0300

    feat: joins 7/10 done

--- gym/scripts/004.sql
+++ gym/scripts/004.sql
@@ -32,3 +32,28 @@ FROM memberships m
 LEFT JOIN enrollments e ON m.id = e.id_membership
 LEFT JOIN students s ON s.id = e.id_student;
 
+-- List student names, enrollment status, and membership plan names. --
+SELECT
+    s.name AS student,
+    e.status,
+    COALESCE(m.name,'Dont have a plan yet.') AS plans
+FROM students s
+LEFT JOIN enrollments e ON s.id = e.id_student
+LEFT JOIN memberships m ON m.id = e.id_membership;
+
+-- List students who have workouts AND an 'active' enrollment status. --
+SELECT
+    s.name AS student,
+    e.status
+FROM students s
+INNER JOIN workouts w ON w.id_student = s.id
+LEFT JOIN enrollments e ON s.id = e.id_student
+WHERE e.status = 'active';
+
+-- Show the student's name and their enrollment start date. --
+SELECT
+    s.name AS student,
+    e.start_date
+FROM students s
+LEFT JOIN enrollments e ON s.id = e.id_student
+WHERE (status != 'cancelled' OR status IS NULL);
