d93b4bc72dc239a9f8e7e1809b9a76ef2c262015
Edu Muller <eduardoaum2007@gmail.com>
2026-02-12 16:59:53 -0300

    feat: Aggregation 6/10 done

--- gym/scripts/005.sql
+++ gym/scripts/005.sql
@@ -8,12 +8,32 @@ FROM students s;
 -- Count how many students currently have an 'active' status. --
 SELECT
     COUNT(*) AS students_active
-FROM students s
-LEFT JOIN enrollments e ON s.id = e.id_student
-GROUP BY e.status 
-HAVING e.status = 'active';
+FROM enrollments
+WHERE status = 'active';
 
 -- Calculate the average price of all available membership plans. --
 SELECT
     ROUND(AVG(m.price), 2) AS average_price
 FROM memberships m;
+
+-- Calculate the total potential revenue if all active students paid their plans today. --
+SELECT
+    SUM(m.price) AS sum_price
+FROM enrollments e
+INNER JOIN memberships m ON e.id_membership = m.id
+WHERE e.status = 'active';
+
+-- Count the number of students for each enrollment status. --
+SELECT
+    status,
+    COUNT(*)
+FROM enrollments e
+GROUP BY status;
+
+-- List each membership plan name and the count of students linked to it. --
+SELECT
+    m.name AS plan_name,
+    COUNT(e.id_student) AS students
+FROM memberships m
+LEFT JOIN enrollments e ON m.id = e.id_membership
+GROUP BY m.name;
