ed73e83c7a42d6f1289631ddca43f0989b612719
Edu Muller <eduardoaum2007@gmail.com>
2026-03-10 21:02:44 -0300

    feat: part1 done

---
+++ dvdrental/part1.sql
@@ -0,0 +1,8 @@
+1-
+SELECT f.title, f.release_year, f.length FROM film f;
+
+2-
+SELECT c.first_name, c.last_name, c.email FROM customer c LIMIT 10;
+
+3-
+SELECT a.first_name, a.last_name FROM actor a;
